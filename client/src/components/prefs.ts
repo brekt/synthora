c00784e1da34f404a981400aa476c3155579ba7c
Mike Burnett <speakoftrees@gmail.com>
2020-06-20 01:06:13 -0400

    can play a chord and also added scale pref (#10)

--- client/src/components/prefs.ts
+++ client/src/components/prefs.ts
@@ -1,36 +1,45 @@
-// import { IotaPrefs, PrefCategories } from '../types';
-
-interface IotaPrefs { [name: string]: string }
-
-interface PrefCategories { [name: string]: string[] }
-
-const prefCategories: PrefCategories = {
-    chords: [
-        'II VI V I',
-        'I IV V I',
-        'I III I IV',
-        'IV V II I'
-    ],
-    drums: [
-        'add',
-        'remove'
-    ],
-    modes: [
-        'ionian',
-        'dorian',
-        'phrygian',
-        'lydian',
-        'mixolydian',
-        'aeolian',
-        'locrian',
-        'harmonic',
-        'melodic',
-        'chromatic'
-    ]
+const notes = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab'];
+const scaleDegrees: ScaleDegrees = {
+    chromatic: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    lydianDominant: [1, 3, 5, 7, 8, 10, 11],
+    major: [1, 3, 5, 6, 8, 10, 12],
+    minor: [1, 3, 4, 6, 8, 9, 11],
+    pelog: [1, 2, 4, 7, 8],
 };
 
-export function getInitialPrefs() : IotaPrefs {
+interface ScaleDegrees {
+    [name: string]: number[];
+}
 
+interface IotaPrefs {
+    [name: string]: string;
+}
+
+interface PrefCategories {
+    [name: string]: string[];
+}
+
+const prefCategories: PrefCategories = {
+    key: [
+        'A',
+        'Bb|A#',
+        'B',
+        'C',
+        'Db|C#',
+        'D',
+        'Eb|D#',
+        'E',
+        'F',
+        'Gb|F#',
+        'G',
+        'Ab|G#',
+    ],
+    progression: ['II VI V I', 'I IV V I', 'I III I IV', 'IV V II I'],
+    drum: ['add', 'remove'],
+    scale: ['major', 'minor', 'lydianDominant', 'chromatic', 'pelog'],
+};
+
+export function getInitialPrefs(): IotaPrefs {
     const initialPrefs: IotaPrefs = {};
 
     for (const category in prefCategories) {
@@ -41,9 +50,19 @@ export function getInitialPrefs() : IotaPrefs {
 }
 
 function getRandomPref(category: string) {
-
-    return prefCategories[category][Math.floor(Math.random() * prefCategories[category].length)]
-
+    return prefCategories[category][
+        Math.floor(Math.random() * prefCategories[category].length)
+    ];
 }
 
-export default getInitialPrefs;
+function getScaleNotes(scale: string) {
+    this.scaleDegrees = scaleDegrees[scale];
+
+    const notes = this.scaleDegrees.map((degree: number) => notes[degree]);
+}
+
+export default {
+    getInitialPrefs,
+    getRandomPref,
+    getScaleNotes,
+};
